87fc1062108efe72bb0ab6338b38d002816f4374
Nober <s0913768710@gmail.com>
2021-06-06 02:08:03 +0800

    Add the trainer.py which define the trainer

---
+++ utils/trainer.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+from .preprocessing import parse_tfRecords
+from tqdm import tqdm
+
+
+
+def Trainer(train, test, model, epoch=5):
+
+
+    train_ds = parse_tfRecords(train).filter(lambda x: x['height'] == x['width'] and x['label'] < 5)
+
+    train_ds = train_ds.shuffle(10000).batch(32)
+
+    test_ds = parse_tfRecords(test).filter(lambda x: x['height'] == x['width'] and x['label'] < 5)
+
+
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    optimizer = tf.keras.optimizers.Adam()
+
+    train_loss = tf.keras.metrics.Mean(name='Train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='Train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='Test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='Test_accuracy')
+
+    @tf.function
+    def train_step(images, labels):
+        with tf.GradientTape() as tape:
+            predictions = model(images, training=True)
+            loss = loss_object(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
+    @tf.function
+    def test_step(images, labels):
+        predictions = model(images, training=True)
+        loss = loss_object(labels, predictions)
+        
+        test_loss(loss)
+        test_accuracy(labels, predictions)
+
+    
+    for epo in range(epoch):
+
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        for record in train_ds:
+            if record['img_raw'].shape[0] == 1:
+                imgs = tf.expand_dims(tf.io.decode_image(record['img_raw'].numpy(), channels=1), axis=0)
+                imgs = tf.cast(imgs, tf.float16)
+                labels = tf.expand_dims(record['labels'], axis=0)
+                print(imgs.shape)
+            else:
+                imgs = tf.map_fn(fn=lambda t: tf.io.decode_image(t.numpy(), channels=1), elems=record['img_raw'], fn_output_signature=tf.uint8)
+                imgs = tf.map_fn(fn=lambda t: tf.cast(t, tf.float16), elems=imgs, fn_output_signature=tf.float16)
+                # imgs = tf.io.decode_image(record['img_raw'].numpy(), channels=1)
+                labels = record['label']
+
+            train_step(imgs, labels)
+
+        for record in test_ds:
+            imgs = tf.io.decode_image(record['img_raw'].numpy(), channels=1)
+            labels = record['label']
+            if len(imgs.shape) <= 3:
+                imgs = tf.expand_dims(imgs, axis=0)
+                labels = tf.expand_dims(labels, axis=0)
+            imgs = tf.cast(imgs, dtype=tf.float16)
+            test_step(imgs, labels)
+
+        print("="*60)
+        print(f'Epoch : {epo}\n train_loss: {train_loss.result()},\n train_accuracy : {train_accuracy.result()}')
+        print(f'test_loss : {test_loss.result()}, \n test_accuracy : {test_accuracy.result()}')
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
